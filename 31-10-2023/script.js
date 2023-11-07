26826f4c90caf94aadb2091574ceb1a543cb2710
MCelesteMassaro <148492224+MCelesteMassaro@users.noreply.github.com>
2023-11-07 18:16:01 +0100

    Add files via upload

---
+++ 31-10-2023/script.js
@@ -0,0 +1,18 @@
+// Creazione dell'array di animali
+const animali = [
+  "cane",
+  "gatto",
+  "elefante",
+  "tigre",
+  "orso",
+  "scimmia",
+  "panda",
+  "quokka",
+];
+
+// Iterazione su ciascun elemento dell'array e stampa in console
+
+console.log(animal);
+//});
+
+//animali.forEach((animal) => {
