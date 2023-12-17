4837c034bee295e1a90bbf349a2535f85a87f20b
MCelesteMassaro <148492224+MCelesteMassaro@users.noreply.github.com>
2023-12-17 19:31:23 +0100

    Add files via upload

---
+++ 15-12-2023/section.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const section.jsx=({title, children}) => (
+<div style={{ marginBottom: '20px' }}>
+    <h2>{Sezione}</h2>    
+    {children}
+  </div>
+);
+
+export default section;
